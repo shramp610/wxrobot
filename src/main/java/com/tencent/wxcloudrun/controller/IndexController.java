4756e3543f11cbe4e3f09ccaa0e191638986c834
zhengxb32214 <zhengxb32214@hundsun.com>
2023-06-11 15:43:29 +0800

    update3

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -2,18 +2,19 @@ package com.tencent.wxcloudrun.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * index控制器
  */
-@Controller
+@RestController
 public class IndexController {
 
   /**
    * 主页页面
    * @return API response html
    */
-  @GetMapping
+  @GetMapping(value = "hello")
   public String index() {
     return "hello，index";
   }
